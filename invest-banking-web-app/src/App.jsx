99ca6c527f59545e7c8bfb405803ca41ef48be3f
Inseok Chang <inni.chang95@gmail.com>
2024-02-29 11:50:22 +0900

    App component wip

--- invest-banking-web-app/src/App.jsx
+++ invest-banking-web-app/src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react"
 import Header from "./components/Header"
+import Result from "./components/Result"
 import UserInput from "./components/UserInput"
 
+const INITIAL_INPUT_VALUES = {
+  initialInvestment: null,
+  annualInvestment: null,
+  expectedReturn: null,
+  duration: null
+}
+
 function App() {
+  const [inputValues, setInputValues] = useState(INITIAL_INPUT_VALUES);
+
+  function handleInputValues(newValues) {
+    setInputValues(newValues);
+  }
+
+
   return (
     <main>
       <Header />
-      <UserInput />
+      <UserInput onChangeValue={handleInputValues}/>
+      <Result />
     </main>
   )
 }
